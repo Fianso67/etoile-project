6d26aa40214012560361910c78415d2baa1d5459
Fianso67 <106553339+Fianso67@users.noreply.github.com>
2022-06-03 10:37:25 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import turtle
+t = turtle.turtle()
+
+   t.forward(150)
+   t.left(144)
+   t.forward(150)
+   t.left(144)
+   t.forward(150)
+   t.left(144)
+   t.forward(150)
+   t.left(144)
+   t.forward(150)
+   t.left(144)
+
+
